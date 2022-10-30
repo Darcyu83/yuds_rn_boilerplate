34ff1576b5db739103ed00641a38931d302bcb45
Darcyu83 <darcy.u83@gmail.com>
2022-10-30 15:51:32 +0900

    Init boilerplate base

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+interface IProps {}
+
+const App = ({}: IProps) => {
+  return (
+    <View style={{}}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
+https://github.com/Darcyu83/yuds_rn_boilerplate.git
